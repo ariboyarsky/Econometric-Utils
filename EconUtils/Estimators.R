de09868f61f4fb08703eb7503dce747053c78b02
Ari Boyarsky <aboyarsky@uchicago.edu>
2018-10-28 17:18:27 -0500

    add iv

--- EconUtils/Estimators.R
+++ EconUtils/Estimators.R
@@ -52,10 +52,8 @@ log.logit <- function(X,Y){
 ##############################################################################
 ##############################################################################
 
-##############################################################################
 # Mutliple Linear Regression
 # Homoskedastic Std Errors (todo: add option for asymptotically consistent SE)
-##############################################################################
 linreg <- function(Y, X, con = 1){
   # get stuff as matricies
   Y <- as.matrix(Y)
@@ -83,3 +81,51 @@ linreg <- function(Y, X, con = 1){
 }
 ##############################################################################
 ##############################################################################
+# IV Regression (2SLS) 
+# Consider adding asymptotically consistent SE
+# ^Doing so yields a memory issue in R with the Angrist data???
+ols.iv <- function(Y,Z,X,controls, con = 1){
+  if (con==1){
+    controls <- as.matrix(cbind(1, controls))
+  }else{
+    controls <- as.matrix(controls) 
+  }
+  X <- as.matrix(X)
+  Z <- as.matrix(Z)
+  Y <- as.matrix(Y)
+  
+  # first stage
+  W <- as.matrix(cbind(controls,Z))
+  
+  # drop any 0 cols
+  W <-W[, colSums(W != 0) > 0]
+  
+  beta.first <- solve(t(W)%*%W, tol = 1e-20)%*%t(W)%*%X
+  pred <- W%*%beta.first
+  
+  colnames(pred) <- c("Z")
+  
+  # second stage
+  if(con==1){
+    pred <- as.matrix(cbind(1,pred,C))
+  }else{
+    pred <- as.matrix(cbind(pred,C))
+  }
+  
+  # drop any 0 cols
+  pred <-pred[, colSums(pred != 0) > 0]
+  
+  
+  beta.iv <- solve(t(pred)%*%pred)%*%t(pred)%*%Y
+  
+  # se
+  p2 <- pred%*%beta.iv
+  n <- NROW(X)
+  sigma2 <- sum((Y-p2)^2) / (nrow(pred)-ncol(pred))
+  SE <- sqrt(diag(solve(t(pred)%*%pred)*sigma2))
+  
+  return(list("beta.iv"=beta.iv, "beta.first"=beta.first, "SE"=SE))
+}
+##############################################################################
+##############################################################################
+
